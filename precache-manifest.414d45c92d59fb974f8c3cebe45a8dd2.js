97ce34216b9775b9d3733ca874bfd8b375a0cb33
melissa <nadia.mm.mariduena@gmail.com>
2020-09-16 22:38:32 +0200

    Updates

---
+++ precache-manifest.414d45c92d59fb974f8c3cebe45a8dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c0f01a582870d0ff0e4582f2c65110",
+    "url": "/react-img-reveal1-gsap/index.html"
+  },
+  {
+    "revision": "c7ca993c688605a66314",
+    "url": "/react-img-reveal1-gsap/static/css/main.87c58149.chunk.css"
+  },
+  {
+    "revision": "94eb1ad2a578805195e4",
+    "url": "/react-img-reveal1-gsap/static/js/2.8e4af051.chunk.js"
+  },
+  {
+    "revision": "7b0d56e16d5c8399a106642d5e2abe41",
+    "url": "/react-img-reveal1-gsap/static/js/2.8e4af051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ca993c688605a66314",
+    "url": "/react-img-reveal1-gsap/static/js/main.f7505be4.chunk.js"
+  },
+  {
+    "revision": "dc5a482c71aa257bf901",
+    "url": "/react-img-reveal1-gsap/static/js/runtime-main.5ab78b52.js"
+  },
+  {
+    "revision": "d0ada4b4c91c94274a0c06da8ae1232d",
+    "url": "/react-img-reveal1-gsap/static/media/people.d0ada4b4.webp"
+  }
+]);
